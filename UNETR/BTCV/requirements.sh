7c87d8b0f92751965c8270bced673999f48e49fc
ahatamizadeh <ahatamizadeh@nvidia.com>
2021-07-13 07:38:36 -0700

    update dependencies
    Signed-off-by: ahatamizadeh <ahatamizadeh@nvidia.com>

--- UNETR/BTCV/requirements.sh
+++ UNETR/BTCV/requirements.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-pip install -q "monai-weekly[nibabel, tqdm, einops]"; pip install --upgrade --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex/; pip install tensorboardX
+pip install monai-weekly==0.6.dev2127;
+pip install nibabel==3.1.1;
+pip install tqdm==4.59.0;
+pip install einops==0.3.0;
+pip install tensorboardX==2.1;
+pip install --upgrade --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex/
