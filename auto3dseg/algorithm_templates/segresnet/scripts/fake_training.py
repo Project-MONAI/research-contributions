0bea0643d88f25fef148f245b6648d59adbe2822
dongy <dongy@nvidia.com>
2022-12-05 07:47:06 -0800

    update repo

--- auto3dseg/algorithm_templates/segresnet/scripts/fake_training.py
+++ auto3dseg/algorithm_templates/segresnet/scripts/fake_training.py
@@ -8,8 +8,7 @@ import yaml
 
 
 def objective(trial):
-    num_images_per_batch = trial.suggest_int("num_images_per_batch", 1, 20)
-    num_patches_per_image = trial.suggest_int("num_patches_per_image", 1, 20)
+    num_images_per_batch = trial.suggest_int("num_images_per_batch", 1, 12)
     num_sw_batch_size = trial.suggest_int("num_sw_batch_size", 1, 40)
     validation_data_device = trial.suggest_categorical(
         "validation_data_device", ["cpu", "gpu"]
@@ -18,13 +17,12 @@ def objective(trial):
     device_factor = 2.0 if validation_data_device == "gpu" else 1.0
 
     try:
-        cmd = f"bash fake_training_script.sh {num_images_per_batch} {num_patches_per_image} {num_sw_batch_size} {validation_data_device}"
+        cmd = f"bash fake_training_script.sh {num_images_per_batch} {num_sw_batch_size} {validation_data_device}"
         _ = subprocess.run(cmd.split(), check=True)
     except:
         print("[error] OOM")
         return (
             float(num_images_per_batch)
-            * float(num_patches_per_image)
             * float(num_sw_batch_size)
             * device_factor
         )
@@ -32,23 +30,20 @@ def objective(trial):
     value = (
         -1.0
         * float(num_images_per_batch)
-        * float(num_patches_per_image)
         * float(num_sw_batch_size)
         * device_factor
     )
 
     dict_file = {}
     dict_file["num_images_per_batch"] = int(num_images_per_batch)
-    dict_file["num_patches_per_image"] = int(num_patches_per_image)
     dict_file["num_sw_batch_size"] = int(num_sw_batch_size)
     dict_file["validation_data_device"] = str(validation_data_device)
     dict_file["value"] = int(value)
 
-    with open("hyper_param_dints.yaml", "a") as out_file:
+    with open("hyper_param_segresnet.yaml", "a") as out_file:
         yaml.dump([dict_file], stream=out_file)
 
     return value
-
 
 os.system("clear")
 
--- auto3dseg/algorithm_templates/segresnet/scripts/fake_training_script_segresnet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=1
-
-python fake_training_script_segresnet.py ${1} ${2} ${3}
