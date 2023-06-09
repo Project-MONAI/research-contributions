e283402e9be8588048edb4867da958248153962a
dongy <dongy@nvidia.com>
2023-06-09 13:16:20 -0700

    update
    Signed-off-by: dongy <dongy@nvidia.com>

--- auto3dseg/algorithm_templates/dints/scripts/search.py
+++ auto3dseg/algorithm_templates/dints/scripts/search.py
@@ -112,7 +112,10 @@ def run(config_file: Optional[Union[str, Sequence[str]]] = None, **override):
     softmax = parser.get_parsed_content("searching#softmax")
 
     # update transforms
-    parser["transforms_train"]["transforms"][9]["num_samples"] = num_patches_per_image
+    for _i in range(len(parser["transforms_train"]["transforms"])):
+        if ("crop" in parser["transforms_train"]["transforms"][_i]["_target_"].lower()
+            and "num_samples" in parser["transforms_train"]["transforms"][_i]):
+            parser["transforms_train"]["transforms"][_i]["num_samples"] = num_patches_per_image
 
     train_transforms = parser.get_parsed_content("transforms_train")
     val_transforms = parser.get_parsed_content("transforms_validate")
